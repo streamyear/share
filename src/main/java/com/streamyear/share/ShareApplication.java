edfc07b7278a259739d07114c1d553de7ffa085f
Beck <2500931932@qq.com>
2018-06-11 21:51:35 +0800

    first commit

---
+++ src/main/java/com/streamyear/share/ShareApplication.java
@@ -0,0 +1,22 @@
+package com.streamyear.share;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@SpringBootApplication
+public class ShareApplication implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareApplication.class, args);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new FastJsonHttpMessageConverter());
+    }
+}
---
+++ src/main/java/com/streamyear/share/controller/index/IndexController.java
@@ -0,0 +1,25 @@
+package com.streamyear.share.controller.index;
+
+import com.streamyear.share.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 首页
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public ModelAndView toIndex(){
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("siteName", "个人生活分享Site");
+
+        User user = new User();
+        user.setId(1);
+        user.setName("流年");
+        mv.addObject("user", user);
+        return mv;
+    }
+}
