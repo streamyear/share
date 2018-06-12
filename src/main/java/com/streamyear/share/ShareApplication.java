1b3b78f2d292e29abff8a3abb000d7c8e326d9b2
streamyear <13524781846@163.com>
2018-06-12 09:32:54 +0800

    集成mybatis, 自动生成文件.

--- src/main/java/com/streamyear/share/ShareApplication.java
+++ src/main/java/com/streamyear/share/ShareApplication.java
@@ -1,6 +1,7 @@
 package com.streamyear.share;
 
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @SpringBootApplication
+@MapperScan("com.streamyear.share.mapper")
 public class ShareApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
--- src/main/java/com/streamyear/share/entity/User.java
+++ src/main/java/com/streamyear/share/entity/User.java
@@ -1,26 +1,43 @@
 package com.streamyear.share.entity;
 
-import java.io.Serializable;
+public class User {
+    private Integer userId;
 
-public class User implements Serializable {
+    private String userName;
 
-    private Integer id;
+    private String password;
 
-    private String name;
+    private String phone;
 
-    public Integer getId() {
-        return id;
+    public Integer getUserId() {
+        return userId;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setUserId(Integer userId) {
+        this.userId = userId;
     }
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
     }
 }
