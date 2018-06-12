0327503c75c6a7c451389d62d24b0533f6804dba
streamyear <13524781846@163.com>
2018-06-12 14:07:37 +0800

    优化

--- src/main/java/com/streamyear/share/mapper/UserMapper.java
+++ src/main/java/com/streamyear/share/mapper/UserMapper.java
@@ -1,17 +1,8 @@
 package com.streamyear.share.mapper;
 
+import com.streamyear.share.common.BaseMapper;
 import com.streamyear.share.entity.User;
 
-public interface UserMapper {
-    int deleteByPrimaryKey(Integer userId);
+public interface UserMapper extends BaseMapper<User> {
 
-    int insert(User record);
-
-    int insertSelective(User record);
-
-    User selectByPrimaryKey(Integer userId);
-
-    int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKey(User record);
 }
